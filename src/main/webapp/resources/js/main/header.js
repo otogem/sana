5b5b57c395ffb65f4fd09626d74b747f98e7bce5
GR <GR@DESKTOP-ODLO1F7>
2022-10-31 17:13:45 +0900

    헤더 ui 정리

--- src/main/webapp/resources/js/main/header.js
+++ src/main/webapp/resources/js/main/header.js
@@ -4,30 +4,28 @@
 $(document).ready(function(){ /*탭메뉴*/
 const button=() => {
 	const burger = document.querySelector('.burger'); // burger class 가져오기
+	const user = document.querySelector('.user>a')
 	burger.addEventListener('click', ()=> { //클릭 이벤트
 		burger.classList.toggle('toggle'); //toggle class가 있으면 제거,없으면 추가
+		if($('.user>a').hasClass("on")) {
+			$('.user>a').removeClass('on')
+			$('.user>a').next().css("display","none");
+		}
 	});
 } 
 
 button();
 
-	$('.user>a').hover(function(){// 마우스를 a태그에 위치할시 div display 펼치고 뺄시 사라지는 코드
-		$(this).addClass('on');
-		$(this).next().css("display","block");
-	}, function(){
-			$(this).removeClass('on');
+	$('.user>a').off().click(function(){// 마우스를 a태그에 위치할시 div display 펼치고 뺄시 사라지는 코드
+		$(this).toggleClass('on');
+		
+		if($(this).hasClass("on")) {
+			$(this).next().css("display","block");
+		}else {
 			$(this).next().css("display","none");
+		}
 
-	})
-	
-	$('.user>div').hover(function(){// 마우스를 div에 위치할시 div display 펼치고 뺄시 사라지는 코드
-		$(this).prev().addClass('on');
-		$(this).css("display","block");
-	}, function(){
-			$(this).prev().removeClass('on');
-			$(this).css("display","none");
-
-	})
+	});
 	
 	// 햄버거 버튼 클릭 이벤트 
 	  var burger = $('.burger');
@@ -45,12 +43,21 @@ button();
 
 	  $(burger).on("click", function () {
 	    if (h == 0) {
-			  $('.ham-con').animate({
+	    	$('.ham-con').on('scroll touchmove mousewheel', function(event) { //스크롤 고정
+	    	  event.preventDefault();
+	    	  event.stopPropagation();
+	    	  return false;
+	    	});
+	    	
+	    	$('html, body').scrollTop(0); // 버튼 클릭시 스크롤 0으로 위치이동
+	    	
+			  $('.ham-con').animate({ //유저 편의성 메뉴바 펼치기
 				  left: '0%'
 			  }, 150);
 			  $(this).addClass('active-1');
 			  h++;
 		  } else if (h == 1) {
+			  $('.ham-con').off('scroll touchmove mousewheel');
 			  $('.ham-con').animate({
 				  left: '-110%'
 			  }, 150);
@@ -58,24 +65,6 @@ button();
 			  h--;
 		  }
 	  });
-	 /*   $(window).resize(function (){
-	    	  // width값을 가져오기
-	    	  var width_size = window.outerWidth;
-	    	  console.log(width_size);
-	    	  // 800 이하인지 if문으로 확인
-	    	  if (width_size > 760) {
-	    	    $('.ham-con').animate({
-	    	    	left: '-100%',
-	    	    	opacity: 1
-	    	    }, 150);
-	    	    $('.burger').removeClass('toggle active-1');
-	    	    h=0;
-	    	  }else if(width_size < 760){
-	    		  $('.ham-con').animate({
-	    			  opacity: 0
-	    		  }, 150);
-	    	  };
-	    	});*/
 	  
 	  $(window).resize(function (){
 		  var width_size = window.innerWidth;
